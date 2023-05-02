f46283ecb495dbab35e77a4d76519d32674240a8
geneningz <geneningz@outlook.com>
2023-05-02 17:43:16 +0800

    feature(api): 构建python flask后端应用

---
+++ apps/flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+# from urllib.parse import unquote
+# from werkzeug.utils import secure_filename
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route("/api/upload", methods=["POST"])
+def upload_file():
+    file = request.files["file"]
+    file.save("uploads/" + file.filename)
+
+    return "File Uploaded!"
+
+
+
+if __name__ == '__main__':
+    app.run()
+
